957f81966297ceb2f3b222148554f3904693b57b
mehdi <mousavi_1986@yahoo.com>
2018-05-21 16:49:31 +0430

    ingredient show

--- src/main/java/com/example/recipe/Controllers/ingredientsController.java
+++ src/main/java/com/example/recipe/Controllers/ingredientsController.java
@@ -1,12 +1,28 @@
 package com.example.recipe.Controllers;
 
 import com.example.recipe.services.recipeServise;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
+@Slf4j
+@Controller
 public class ingredientsController {
 
     recipeServise recipeServise;
     public ingredientsController(recipeServise recipeServise) {
+        this.recipeServise=recipeServise;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId,Model model){
+        log.debug("getting Ingredients list for recipe ID: "+ recipeId);
+        model.addAttribute("recipe", recipeServise.findCommandById(Long.valueOf(recipeId)));
+        return "recipe/ingredients/list";
     }
 }
--- src/test/java/com/example/recipe/Controllers/ingredientsControllerTest.java
+++ src/test/java/com/example/recipe/Controllers/ingredientsControllerTest.java
@@ -1,12 +1,24 @@
 package com.example.recipe.Controllers;
 
+import com.example.recipe.commands.RecipeCommand;
 import com.example.recipe.services.recipeServise;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 public class ingredientsControllerTest {
 
@@ -19,7 +31,24 @@ public class ingredientsControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         controller=new ingredientsController(redipeServise);
+        mockMvc= MockMvcBuilders.standaloneSetup(controller).build();
+
     }
 
+    @Test
+    public void testListIngredients() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(redipeServise.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredients/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        //then
+        verify(redipeServise, times(1)).findCommandById(anyLong());
+    }
 
 }
